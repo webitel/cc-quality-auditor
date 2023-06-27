d3d954b6b546f311653e6106588dfc17d418b992
lizacoma <my.skin00@gmail.com>
2023-06-27 18:44:04 +0300

    fix: small refactoring in API [WTEL-3497]

--- src/app/api/APIRepository.js
+++ src/app/api/APIRepository.js
@@ -26,35 +26,31 @@ const fieldsToSend = [
   'questions',
 ];
 
-const itemResponseHandler = (response) => {
-  const newResponseObject = {
-    ...response,
-    questions: response.questions.map((question) => {
-      if (question.type === EngineAuditQuestionType.Score) {
-        return {
-          ...question,
-          max: question.max || 1,
-          min: question.min || 0,
-          required: question.required || false,
-          question: question.question || '',
-        };
-      }
-      if (question.type === EngineAuditQuestionType.Option) {
-        return {
-          ...question,
-          options: question.options.map((option) => ({
-            ...option,
-            name: option.name || '',
-            score: option.score || 0,
-          })),
-        };
-      }
-      return question;
-    }),
-  };
-
-  return newResponseObject;
-};
+const itemResponseHandler = (response) => ({
+  ...response,
+  questions: response.questions.map((question) => {
+    if (question.type === EngineAuditQuestionType.Score) {
+      return {
+        ...question,
+        max: question.max || 1,
+        min: question.min || 0,
+        required: question.required || false,
+        question: question.question || '',
+      };
+    }
+    if (question.type === EngineAuditQuestionType.Option) {
+      return {
+        ...question,
+        options: question.options.map((option) => ({
+          ...option,
+          name: option.name || '',
+          score: option.score || 0,
+        })),
+      };
+    }
+    return question;
+  }),
+});
 
 const getAuditList = async (params) => {
   const {
