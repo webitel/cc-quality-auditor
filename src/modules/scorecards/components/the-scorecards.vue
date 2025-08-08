f4c05459d46e1ef2bbf190c9714588629b48de67
Yehor Hlukhov <egorgluhov68@gmail.com>
2025-08-08 18:14:09 +0300

    refactor: switcher usage [WTEL-7381](https://webitel.atlassian.net/browse/WTEL-7381)

--- src/modules/scorecards/components/the-scorecards.vue
+++ src/modules/scorecards/components/the-scorecards.vue
@@ -100,8 +100,8 @@
             <template #state="{ item, index }">
               <wt-switcher
                 :disabled="!hasEditAccess"
-                :value="item.enabled"
-                @change="patchProperty({ item, index, prop: 'enabled', value: $event })"
+                :model-value="item.enabled"
+                @update:model-value="patchProperty({ item, index, prop: 'enabled', value: $event })"
               />
             </template>
             <template #actions="{ item }">
