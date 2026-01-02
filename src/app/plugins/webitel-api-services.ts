5f9eb87a6bbe32f54cd86b6140e4fb8d1a261e75
volodymyr <volodymyr.dechtiaruk@gmail.com>
2026-01-02 15:19:08 +0200

    refactor: import i18n extension redone [WTEL-7696](https://webitel.atlassian.net/browse/WTEL-7696)

--- src/app/plugins/webitel-api-services.ts
+++ src/app/plugins/webitel-api-services.ts
@@ -1,7 +1,7 @@
 import { setConfig } from '@webitel/api-services';
 import eventBus from '@webitel/ui-sdk/src/scripts/eventBus';
 
-import i18n from '../locale/i18n.js';
+import i18n from '../locale/i18n';
 
 // Configure api-services
 // Note: Locale messages are automatically merged by setConfig when i18n is provided
