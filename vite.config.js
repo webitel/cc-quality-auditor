546a179f5007b86f6b360d900c655716e2cb354c
Roman Zaritskyi <r.zaritskyi@steel.mil.ua>
2026-02-23 21:05:51 +0200

    fix: codestyle in vite config file [WTEL-8897](https://webitel.atlassian.net/browse/WTEL-8897)

--- vite.config.js
+++ vite.config.js
@@ -1,44 +1,37 @@
-import vue from '@vitejs/plugin-vue';
-import { resolve } from 'path';
-import { defineConfig } from 'vite';
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
-import vueDevTools from 'vite-plugin-vue-devtools';
+import vue from "@vitejs/plugin-vue";
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
+import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vitejs.dev/config/
 export default () => {
 	return defineConfig({
-		base: '/audit',
+		base: "/audit",
 		server: {
 			// host: true,  // uncomment me to enable localhost access by IP (including from other devices in the network)
 		},
 		css: {
 			preprocessorOptions: {
 				scss: {
-					api: 'modern',
+					api: "modern",
 				},
 			},
 		},
 		optimizeDeps: {
-			include: [
-				'clipboard-copy',
-				'deep-equal',
-				'deepmerge',
-			],
+			include: ["clipboard-copy", "deep-equal", "deepmerge"],
 		},
 		resolve: {
 			// deduping compat is needed for audit form to prevent vue instance duplicating
-			dedupe: [
-				'vue',
-				'@vue/compat',
-			],
+			dedupe: ["vue", "@vue/compat"],
 			alias: {
-				vue: '@vue/compat',
-				'@': resolve(__dirname, 'src'),
-				'@aliasedDeps/api-services/axios': resolve(
+				vue: "@vue/compat",
+				"@": resolve(__dirname, "src"),
+				"@aliasedDeps/api-services/axios": resolve(
 					__dirname,
-					'src/app/api/instance',
+					"src/app/api/instance",
 				),
-				lodash: 'lodash-es',
+				lodash: "lodash-es",
 			},
 		},
 		plugins: [
@@ -54,30 +47,24 @@ export default () => {
 			// https://www.npmjs.com/package/vite-plugin-node-polyfills
 			nodePolyfills({
 				// are needed for csv-parse
-				include: [
-					'buffer',
-					'stream',
-					'process',
-				],
+				include: ["buffer", "stream", "process"],
 				globals: {
 					Buffer: true, // can also be 'build', 'dev', or false
 					process: true, // csv stringify
 				},
 			}),
 			vueDevTools({
-				launchEditor: 'webstorm',
+				launchEditor: "webstorm",
 			}),
 		],
 		test: {
 			globals: true,
 			coverage: {
 				enabled: true,
-				reporter: 'json',
+				reporter: "json",
 			},
-			environment: 'happy-dom',
-			setupFiles: [
-				'./tests/config/config.js',
-			],
+			environment: "happy-dom",
+			setupFiles: ["./tests/config/config.js"],
 		},
 	});
 };
