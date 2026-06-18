01ba9417af0f9293b03b12e61440f6149f293bc7
Daniil Lohvinov <32868304+dlohvinov@users.noreply.github.com>
2026-06-18 17:30:53 +0300

    fix(dark-mode): provide darkMode from app.vue [WTEL-9799](https://webitel.atlassian.net/browse/WTEL-9799)

--- src/app.vue
+++ src/app.vue
@@ -3,6 +3,18 @@
   <router-view />
 </template>
 
+<script
+  lang="ts"
+  setup
+>
+import { computed, provide } from 'vue';
+import { useStore } from 'vuex';
+
+const store = useStore();
+const darkMode = computed(() => store.getters['appearance/DARK_MODE']);
+provide('darkMode', darkMode);
+</script>
+
 <style>
 #app {
   height: 100%;
